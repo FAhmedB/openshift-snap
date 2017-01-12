d53d9a09fa7f76a2eb99450ea3bf516a91c626bb
FAhmedB <bouafif.ahmed@gmail.com>
2017-01-12 17:45:51 +0100

    new

---
+++ src/main/java/com/directions/OpenshiftSnapApplication.java
@@ -0,0 +1,12 @@
+package com.directions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenshiftSnapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenshiftSnapApplication.class, args);
+	}
+}
