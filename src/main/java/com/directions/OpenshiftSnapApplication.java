45c657a1aa7878bb85c2c7e4bc1cdcc5c3f4e20b
Ahmed <Ahmed>
2017-01-16 12:23:15 +0100

    changed the main class

--- src/main/java/com/directions/OpenshiftSnapApplication.java
+++ src/main/java/com/directions/OpenshiftSnapApplication.java
@@ -1,12 +1,26 @@
 package com.directions;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
 @SpringBootApplication
-public class OpenshiftSnapApplication {
+public class OpenshiftSnapApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(OpenshiftSnapApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(OpenshiftSnapApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(OpenshiftSnapApplication.class);
+    }
 }
